1f159299afb383ab11800d972f9dec9fe0ad3325
ShadowConYT <iamajay6122@gmail.com>
2023-11-17 21:59:31 +0530

    Update App.css and Lander.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar';
 import Lander from './components/Lander';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Lander />
     </div>
   );
--- src/components/Lander.jsx
+++ src/components/Lander.jsx
@@ -3,67 +3,19 @@ import Basmati from '../assets/Basmati.webp';
 import IdlyRice from '../assets/IdlyRice.jpg';
 import Ponni from '../assets/Ponni.jpeg';
 import Seeragasamba from '../assets/Seeragasamba.jpg';
+import Navbar from "./Navbar";
+import LanderVideo from '../assets/LanderVideo.mp4';
  
 export default function Lander() {
   return (
-    <Carousel
-      className="rounded-xl"
-      navigation={({ setActiveIndex, activeIndex, length }) => (
-        <div className="absolute bottom-0 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-          {new Array(length).fill("").map((_, i) => (
-            <span
-              key={i}
-              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                activeIndex === i ? "w-8 bg-black" : "w-4 bg-white/50"
-              }`}
-              onClick={() => setActiveIndex(i)}
-            />
-          ))}
-        </div>
-      )}
-    >
-        <div className="flex justify-evenly mt-16 w-full h-full object-cover">
-            <div className="flex flex-col items-center justify-center h-96">
-                <h1 className="text-4xl font-bold text-gray-800">Welcome to Rice Mill</h1>
-                <p className="text-gray-600">We are the best rice mill in the world</p>
-            </div>
-            <div>
-                <img src={Ponni} className="w-96"/>
-            </div>
-        </div>
-
-        <div className="flex justify-evenly mt-16">
-            <div className="flex flex-col items-center justify-center h-96">
-                <h1 className="text-4xl font-bold text-gray-800">Welcome to Rice Mill</h1>
-                <p className="text-gray-600">We are the best rice mill in the world</p>
-            </div>
-            <div>
-                <img src={IdlyRice} className="w-96"/>
-            </div>
-        </div>
-
-        <div className="flex justify-evenly mt-16">
-            <div className="flex flex-col items-center justify-center h-96">
-                <h1 className="text-4xl font-bold text-gray-800">Welcome to Rice Mill</h1>
-                <p className="text-gray-600">We are the best rice mill in the world</p>
-            </div>
-            <div>
-                <img src={Seeragasamba} className="w-96"/>
-            </div>
-        </div>
-
-        <div className="flex justify-evenly mt-16">
-            <div className="flex flex-col items-center justify-center h-96">
-                <h1 className="text-4xl font-bold text-gray-800">Welcome to Rice Mill</h1>
-                <p className="text-gray-600">We are the best rice mill in the world</p>
-            </div>
-            <div>
-                <img src={Basmati} className="w-96"/>
-            </div>
-        </div>
-
-
-    </Carousel>
+    <>
+      <video src={LanderVideo} autoPlay muted loop className="lv absolute h-screen object-cover -z-10"/>
+      <Navbar />
+      <div className="mainTxt flex flex-col items-center align-middle justify-center z-10">
+        <h1 className="text-8xl text-white font-bold">Jivan Grains</h1>
+        <p className="text-white text-3xl">Harvested with Love, Served with Tradition</p>
+      </div>
+  </>            
   )
 }
 
