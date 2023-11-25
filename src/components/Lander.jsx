08636fc3d682cbdec74ceec6db1da0b09ebbc6dd
ShadowConYT <iamajay6122@gmail.com>
2023-11-25 11:51:54 +0530

    Add home page buttons

--- src/components/Lander.jsx
+++ src/components/Lander.jsx
@@ -9,6 +9,14 @@ export default function Lander() {
       <div className="mainTxt flex flex-col items-center align-middle justify-center z-10">
         <h1 className="text-3xl md:text-8xl text-white font-bold font-mont uppercase tracking-wider">Jivan Grains</h1>
         <p className="text-white text-lg md:text-3xl">"Harvested with Love, Served with Tradition"</p>
+        <div className="flex space-x-8 mt-5">
+          <button id="butt" class="btn-shine">
+            <span>Our Products</span>
+          </button>
+          <button id="butt" class="btn-shine">
+            <span>Contact Us</span>
+          </button>
+        </div>
       </div>
   </div>            
   )
